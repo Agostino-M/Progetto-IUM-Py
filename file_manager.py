770db4f97976fc72eed4b827fb3e731634fda279
Agostino Messina <agostino.messina@edu.unito.it>
2022-04-27 23:10:41 +0200

    Create read matrix from file and minor fix

--- file_manager.py
+++ file_manager.py
@@ -1,8 +1,20 @@
 from typing import Final
 
-FILE_NAME: Final = "input.txt"
+PATTERN_FILE_NAME: Final = "input_pattern.txt"
+MATRIX_FILE_NAME: Final = "input_matrix.txt"
 
 
 def read_pattern_in_file():
-    with open(FILE_NAME, 'r') as f:
-        return [[int(num) for num in line.split(' ')] for line in f if line.strip() != ""]
+    try:
+        with open(PATTERN_FILE_NAME, 'r') as f:
+            return [[int(num) for num in line.split(' ')] for line in f if line.strip() != ""]
+    except:
+        print("An error occurred while reading the", PATTERN_FILE_NAME, "file.")
+
+
+def read_matrix_in_file():
+    try:
+        with open(MATRIX_FILE_NAME, 'r') as f:
+            return [[int(num) for num in line.split(' ')] for line in f if line.strip() != ""]
+    except:
+        print("An error occurred while reading the", MATRIX_FILE_NAME, "file.")
--- main.py
+++ main.py
@@ -1,34 +1,27 @@
-from file_manager import read_pattern_in_file
+from file_manager import read_pattern_in_file, read_matrix_in_file
 from algorithm import rotate_matrix, print_matrix, find_pattern
 
 
 def main():
-    # the map we need to search
-    matrix = [
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0, 0],
-        [1, 0, 0, 0, 1, 0, 0]
-    ]
+    # get the original matrix
+    matrix = read_matrix_in_file()
 
+    # get the pattern to compare with the matrix
     pattern = read_pattern_in_file()
-    print_matrix(pattern)
+    # print_matrix(pattern)
 
-    found = find_pattern(matrix, pattern)
+    found = find_paCreattern(matrix, pattern)
     attempt = 0
     while not found and attempt < 3:
         pattern = rotate_matrix(pattern)
         found = find_pattern(matrix, pattern)
-        print("A:", attempt)
-        print_matrix(pattern)
+        # print("Attempt: ", attempt)
+        # print_matrix(pattern)
         attempt += 1
 
-    print(found)
+    print("Found: ", found)
     if found:
         print("Attempt: ", attempt)
-
-
-# print_matrix(rotate_matrix(pattern))
 
 
 if __name__ == "__main__":
